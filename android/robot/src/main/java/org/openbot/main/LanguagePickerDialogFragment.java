0b88a0fa1a7a964c96bf41d77832ddf1c3e29572
Matthias Mueller <thias15@web.de>
2026-07-22 21:50:24 -0700

    Fix language-change flicker with a branded transition screen

--- android/robot/src/main/java/org/openbot/main/LanguagePickerDialogFragment.java
+++ android/robot/src/main/java/org/openbot/main/LanguagePickerDialogFragment.java
@@ -1,9 +1,12 @@
 package org.openbot.main;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import org.jetbrains.annotations.NotNull;
@@ -14,6 +17,8 @@ import org.openbot.utils.LocaleUtils;
 public class LanguagePickerDialogFragment extends DialogFragment {
 
   private DialogLanguagePickerBinding binding;
+  private String pendingTag;
+  private boolean tagPending = false;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -32,11 +37,27 @@ public class LanguagePickerDialogFragment extends DialogFragment {
             LocaleUtils.getLanguageOptions(requireContext()),
             LocaleUtils.getCurrentTag(),
             option -> {
-              LocaleUtils.applyLocale(option.getTag());
+              // Let the dialog window fully close before starting the loading screen,
+              // otherwise their transitions race each other.
+              pendingTag = option.getTag();
+              tagPending = true;
               dismiss();
             }));
 
     binding.dismiss.setOnClickListener(v -> dismiss());
     return binding.getRoot();
+  }
+
+  @Override
+  public void onDismiss(@NonNull DialogInterface dialog) {
+    super.onDismiss(dialog);
+    if (tagPending) {
+      tagPending = false;
+      Intent intent = new Intent(requireActivity(), LanguageApplyingActivity.class);
+      intent.putExtra(LanguageApplyingActivity.EXTRA_LANGUAGE_TAG, pendingTag);
+      requireActivity().startActivity(intent);
+      requireActivity()
+          .overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
   }
 }
